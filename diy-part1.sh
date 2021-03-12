4788110eb58d2afb16494e19d6482b3c3ca1f926
wx1wxc <wx1wxc@users.noreply.github.com>
2021-03-12 08:36:09 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter package/openappfilter
+git clone https://github.com/xkstudio/luci-app-pptp-server.git package/pptp
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/siryle1213/luci-app-fileassistant.git package/fileassistant
+git clone https://github.com/tty228/luci-app-serverchan package/serverchan
