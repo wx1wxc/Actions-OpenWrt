5f40b6eecd36af8e9b23e35855d8d98e4532cde7
wx1wxc <wx1wxc@users.noreply.github.com>
2022-06-29 16:57:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings #取消系统默认密码
 # sed -i 's/mosdns_neo/mosdns/g' openwrt/feeds/kenzok/luci-app-mosdns/Makefile  #mosdns修复
 # sed -i 's/mosdns_neo/mosdns/g' openwrt/feeds/kenzok/mosdns/Makefile
-rm -rf openwrt/feeds/packages/net/mosdns && cp -rf openwrt/feeds/kenzok/mosdns openwrt/feeds/packages/net/mosdns
-sed -i 's/mosdns_neo/mosdns/g' openwrt/feeds/kenzok/luci-app-mosdns/Makefile
-sed -i 's/mosdns_neo/mosdns/g' openwrt/feeds/kenzok/mosdns/Makefile
-sed -i 's/mosdns_neo/mosdns/g' openwrt/feeds/packages/net/mosdns/Makefile
+rm -rf feeds/packages/net/mosdns && cp -rf feeds/kenzok/mosdns feeds/packages/net/mosdns
+sed -i 's/mosdns_neo/mosdns/g' feeds/kenzok/luci-app-mosdns/Makefile
+sed -i 's/mosdns_neo/mosdns/g' feeds/kenzok/mosdns/Makefile
+sed -i 's/mosdns_neo/mosdns/g' feeds/packages/net/mosdns/Makefile
